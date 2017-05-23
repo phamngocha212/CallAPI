a3222e550acc768ec16266232619a2a27b4f015c
Pham Ngoc <phamngocha212@gmail.com>
2017-05-23 17:11:49 +0700

    Add all file

---
+++ CallAPI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyWebRequest api = new MyWebRequest("http://uat.sgbbc.com.sg/api/list-auctions", "POST", "status=2&token=zVjk98qEsZqFeJmHYxKqVBKmA20oYDwlImHXRuEoN3M&uid=1257");
+            Console.WriteLine(api.GetResponse());
+
+            Console.ReadLine();
+        }
+    }
+}
